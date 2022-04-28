6864cebf7ce74d1c01936577cf8a9ca44f96ce49
Nidal2022 <nidal.mhamdi93@gmail.com>
2022-04-29 00:46:26 +0100

    version finale

--- src/App.js
+++ src/App.js
@@ -34,8 +34,56 @@ const App =() => {
     Title:'John Wick',
     Released:'19 September 2014',
     Genre:'Action, Police, Thriller',
-    Poster:'https://i.scdn.co/image/ab67616d0000b273282ffbad2d2e6d7df2d3b355',
+    Poster:'https://i.egycdn.com/pic/WmFwZndlY21UWWp2TnBqRWNtYm1MTnBUcHZtbQ.jpg',
     Rate:100,
+    
+  },
+  {
+    Id:Math.random(),
+    Title:'The Batman',
+    Released:'19 January 2022',
+    Genre:'Action, Crime, Drama',
+    Poster:'https://i.egycdn.com/pic/WmFwZndlY212bXBZWXZtRW12RWNjdHdsbXZtTE56UA.jpg',
+    Rate:81,
+    
+  }
+  ,
+  {
+    Id:Math.random(),
+    Title:'Ambulance',
+    Released:'02 February 2022',
+    Genre:'Action, Police, Thriller',
+    Poster:'https://i.egycdn.com/pic/WmFwZndlY212bVlURW1qbXBFRWNtRW1wdm1tbG1URW12.jpg',
+    Rate:63,
+    
+  } ,
+  {
+    Id:Math.random(),
+    Title:'The Northman',
+    Released:'09 Mars 2022',
+    Genre:'Action, Police, Thriller',
+    Poster:'https://i.egycdn.com/pic/WmFwZndlY212bW12Y3ZObXZjdmNFY21HTm1tYndUZk5n.jpg',
+    Rate:79,
+    
+  }
+  ,
+  {
+    Id:Math.random(),
+    Title:"Fortress: Sniper's Eye",
+    Released:'25 December 2020',
+    Genre:'Action, Police, Thriller',
+    Poster:'https://i.egycdn.com/pic/WmFwZndlY212bXZtb3ROWUVFY3dUbXZOalBtWVRq.jpg',
+    Rate:20,
+    
+  }
+  ,
+  {
+    Id:Math.random(),
+    Title:'Wrath of Man ',
+    Released:'25 July 2021',
+    Genre:'Action, Police, Thriller',
+    Poster:'https://i.egycdn.com/pic/WmFwZndlY21Zam1wbW1ZYnZSY21FbW1hbWJ2TmptbWE.jpg',
+    Rate:71,
     
   }]);
   const [searchByValue, setsearchByValue] = useState("");
